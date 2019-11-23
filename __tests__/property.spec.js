925f131c57d2e5245bf955ed65f035c1972a4028
Kaio Cesar <tecnico.kaio@gmail.com>
2019-11-23 11:00:04 -0300

    add mocha and chai

---
+++ __tests__/property.spec.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Property = require('../src/controllers/PropertyController')
+const expect = require('chai').expect
+
+describe('Property module', () => {
+	describe('"up"', () => {
+		it('Should export a function', () => {
+			expect(() => {}).to.be.a('function')	
+		})
+	})
+})
--- __tests__/routes.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest')
-const server = require('../src/server')
-
-request(server)
-    .get('/property')
-    .expect('Content-Type', '/json/')
-    .expect('Content-Length', '15')
-    .expect(200)
-
